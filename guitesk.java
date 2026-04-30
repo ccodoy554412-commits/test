3325f881d1f0ce5f3ee6f71fc8a45540334a6367
C.Charles <c.codoy.554412@umindanao.edu.ph>
2026-05-01 00:38:19 +0800

    mit

--- guitesk.java
+++ guitesk.java
@@ -1,17 +1,11 @@
 package test;
 
-import java.util.Scanner;
-import javax.swing.*;
 
-public class guitesk {
+
+public class guitesk{
 
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("Pig label");
-		frame.setSize(400,300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JLabel kilo = new JLabel("Enter number");
-
 	}
-
+	
 }
